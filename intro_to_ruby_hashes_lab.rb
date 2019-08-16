36d1fdf9304251f8bd2c543ce86e8c29791f2519
Matt Pitner <mattpitner09@gmail.com>
2019-08-16 21:12:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,11 +20,13 @@ def monopoly_with_second_tier
 	  :railroads=>
 	  {
 	    :pieces=>4,
+	   
+	    :names => {
+	  },
+	  
 	    :rent_in_dollars=> {
 	      
-	    },
-	    :names => {
-	  }
+	    }
 	  
 	    
 	  }
